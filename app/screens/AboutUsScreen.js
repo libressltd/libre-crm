b2201ed7314013e4815483cb9537d2e1a0f69534
iConcept <iconcept@MacBook-Pro-2.local>
2017-06-11 21:29:47 +0700

    Release 1.0.4

--- app/screens/AboutUsScreen.js
+++ app/screens/AboutUsScreen.js
@@ -6,8 +6,8 @@
 
 import React, {Component} from 'react';
 import { Container, Header, Title, Content, Footer, FooterTab, Button, Left, Right, Body, Icon, StyleProvider, Item, Input, Label, Form, Text, List, ListItem } from 'native-base';
-import getTheme from '../../native-base-theme/components';
-import material from '../../native-base-theme/variables/platform';
+import getTheme from '../../../native-base-theme/components';
+import material from '../../../native-base-theme/variables/platform';
 
 class AboutUsScreen extends Component {
     render() {
