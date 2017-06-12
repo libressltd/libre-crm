18accea172f69d94999344845580b4a520acd846
iConcept <iconcept@MacBook-Pro-2.local>
2017-06-12 16:56:04 +0700

    Add new file to display

---
+++ addEjectScript.js
@@ -0,0 +1,12 @@
+const printMessage = require("print-message");
+
+printMessage(
+  [
+    "libre-crm already installed",
+    "Copy customize & Config to your folder to make it run",
+  ],
+  {
+    color: "yellow",
+    borderColor: "green"
+  }
+);
